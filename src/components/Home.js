161fbc13aad5b4ef2926435f78f4005e466ad57b
Kalpak <kalpakto26@gmail.com>
2022-07-12 22:11:49 +0530

    Item adding modal added, item map displayed without styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ import { Box } from '@mui/material';
 
 const Home = () => {
 	return (
-		<Box sx={{display: 'flex', flexDirection: 'column'}}>
+		<Box sx={{display: 'flex', flexDirection: 'column', width: '100vw', height: '100vh', overflow: 'hidden', flexGrow: 1, position: 'relative'}}>
 			<Header />
 			<Content />
 			<Footer />
--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -1,9 +1,123 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Box, Button, FormControl, FormHelperText, MenuItem, Select, Typography } from '@mui/material';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
+import NumberFormat from 'react-number-format';
 
 const ItemDetails = () => {
-  return (
-	<div>ItemDetails</div>
-  )
-}
+	const [dialogOpen, setDialogOpen] = useState(false);
+	const [currentItem, setCurrentItem] = useState({ name: '', category: 'veg', value: 0 });
+	const [items, setItems] = useState([]);
 
-export default ItemDetails
+	const handleClickOpen = () => {
+		setDialogOpen(true);
+	};
+
+	const handleClose = () => {
+		setDialogOpen(false);
+	};
+
+	const handleAddItem = () => {
+		console.log(currentItem);
+		setItems((allCurrentItems) => {
+			return [...allCurrentItems, currentItem];
+		});
+		setDialogOpen(false);
+	};
+
+	return (
+		<>
+			<Box>
+				<Button variant='outlined' onClick={handleClickOpen}>
+					Add Item
+				</Button>
+				<Dialog open={dialogOpen} onClose={handleClose} maxWidth='xs'>
+					<DialogTitle>Add an item!</DialogTitle>
+					<DialogContent>
+						<DialogContentText>Please add an item which the gobblers gobbled!</DialogContentText>
+						<Typography variant='body2' sx={{ marginBlockStart: '2rem', marginBlockEnd: '0.5rem' }}>
+							Item Name
+						</Typography>
+						<TextField
+							autoFocus
+							margin='none'
+							id='itemName'
+							label='Item Name'
+							type='text'
+							variant='outlined'
+							fullWidth
+							value={currentItem.name}
+							onChange={(e) =>
+								setCurrentItem((prevItem) => {
+									return { ...prevItem, name: e.target.value };
+								})
+							}
+						/>
+						<Typography variant='body2' sx={{ marginBlock: '0.5rem' }}>
+							Item Price
+						</Typography>
+						<NumberFormat
+							customInput={TextField}
+							value={currentItem.value}
+							displayType={'input'}
+							decimalScale={2}
+							prefix={'₹ '}
+							allowNegative={false}
+							onValueChange={(values) => {
+								const { floatValue } = values;
+								setCurrentItem((prevItem) => {
+									return { ...prevItem, value: floatValue };
+								});
+							}}
+							size={'large'}
+							placeholder='Item Price'
+							fullWidth
+							disabled={false}
+						></NumberFormat>
+						<Typography variant='body2' sx={{ marginBlock: '0.5rem' }}>
+							Item Category
+						</Typography>
+						<FormControl fullWidth>
+							<Select
+								labelId='item-category'
+								id='item-category'
+								value={currentItem.category}
+								onChange={(e) => {
+									setCurrentItem((prevItem) => {
+										return { ...prevItem, category: e.target.value };
+									});
+								}}
+								fullWidth
+							>
+								<MenuItem value={'veg'}>Vegetarian</MenuItem>
+								<MenuItem value={'nonVeg'}>Non-Vegetarian</MenuItem>
+								<MenuItem value={'drinks'}>Drinks</MenuItem>
+								<MenuItem value={'others'}>Others</MenuItem>
+							</Select>
+							<FormHelperText>Select category of the item</FormHelperText>
+						</FormControl>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={handleClose}>Cancel</Button>
+						<Button onClick={handleAddItem}>Add</Button>
+					</DialogActions>
+				</Dialog>
+			</Box>
+			<Box>
+				{items.map((item) => (
+					<Box key={item.name}>
+						<Typography>{item.name}</Typography>
+						<Typography>{item.value}</Typography>
+						<Typography>{item.category}</Typography>
+					</Box>
+				))}
+			</Box>
+		</>
+	);
+};
+
+export default ItemDetails;
